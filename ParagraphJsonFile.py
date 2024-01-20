5f0dbf2e8950efc9c78cdfcad6361546eb912adc
MorphiniIsland <150484931+MorphiniIsland@users.noreply.github.com>
2024-01-20 16:08:04 +0800

    1

---
+++ ParagraphJsonFile.py
@@ -0,0 +1,23 @@
+import json
+
+# Step 1: Read the JSON file
+json_file_path = '/Users/morphini/Desktop/FYP/Coding/FYP_Digital_Soul_Coding_2024/JsonFiles'
+with open(json_file_path, 'r') as json_file:
+    # Step 2: Extract the relevant information
+    data = json.load(json_file)
+
+
+# Create a dictionary with the information you want to save
+output_data = data
+
+# Convert the dictionary to a JSON string
+output_json_string = json.dumps(output_data, indent=2)
+
+# Write the JSON string to a file
+with open('inputparagraphed.json', 'w') as file:
+    file.write(output_json_string)
+
+# Print the model's reply
+print(data)
+
+
